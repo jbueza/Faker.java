1cfa571f41aa2d044a998bafc8c207691ac46e2d
Jaime Bueza <jbueza@gmail.com>
2013-07-05 20:59:44 -0700

    Add generic test

--- test/FakerTest.java
+++ test/FakerTest.java
@@ -1,16 +1,13 @@
+import com.github.jbueza.Faker.Generators.LoremIpsumGenerator;
 import org.junit.Test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jbueza
- * Date: 2013-06-19
- * Time: 6:14 PM
- * To change this template use File | Settings | File Templates.
- */
 public class FakerTest {
 
     @Test
-    public void testConstructor() {
+    public void testLoremIpsumGeneratorConstructor() {
+
+        LoremIpsumGenerator lorem = new LoremIpsumGenerator();
+
 
     }
 
