518df5dba8c054f1612e2d74cc5148e9f8ecd082
Jaime Bueza <jbueza@gmail.com>
2013-07-05 20:59:58 -0700

    API spec

--- src/com/github/jbueza/Faker/Faker.java
+++ src/com/github/jbueza/Faker/Faker.java
@@ -1,8 +1,15 @@
 package com.github.jbueza.Faker;
 
-
+import com.github.jbueza.Faker.Generators.LoremIpsumGenerator;
 
 public class Faker {
 
+    public LoremIpsumGenerator LoremIpsum() {
+        return new LoremIpsumGenerator();
+    }
+
+    public Company Company() {
+
+    }
 
 }
